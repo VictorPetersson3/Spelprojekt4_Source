f061341d2e8cc136053ad50adcc1ff4b9acfa98f
Avyky <Andre.bruce@live.com>
2021-04-01 15:31:43 +0200

    Update CameraBehavoir.cpp

--- Game/CameraBehavoir.cpp
+++ Game/CameraBehavoir.cpp
@@ -33,7 +33,7 @@ void CameraBehavoir::AddForce(const CommonUtilities::Vector2f aForceToAdd)
 
 void CameraBehavoir::CenterCamera()
 {
-	const CommonUtilities::Vector2f playerPos = myPlayerToFollow->GetPosition() + (myPlayerToFollow->GetCurrentVelocity().GetNormalized() * 0.05f);
+	const CommonUtilities::Vector2f playerPos = myPlayerToFollow->GetPosition() + (myPlayerToFollow->GetCurrentVelocity().GetNormalized() * 0.025f);
 	if (playerPos != CommonUtilities::Vector2f::Zero())
 	{
 		const CommonUtilities::Vector2f dir = playerPos - myPosition;	
