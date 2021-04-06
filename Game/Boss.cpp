60e92d903fef881a688aa7137f2dfcb3dbc572ab
Avyky <Andre.bruce@live.com>
2021-04-06 09:56:21 +0200

    Added structure for boss

---
+++ Game/Boss.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+#include "Boss.h"
+void Boss::Init(const std::shared_ptr<Player> aPlayer) {
+
+
+}
+
+void Boss::Update(const float aDt)
+{
+
+}
+
+void Boss::Render(std::shared_ptr<Camera> aCamera)
+{
+
+}
+
+void Boss::Move()
+{
+
+}
+
+CommonUtilities::Vector2f Boss::PickPositionToMoveTowards()
+{
+	return CommonUtilities::Vector2f();
+}
