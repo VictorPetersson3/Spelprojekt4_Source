a7f894683e3f4e34ef63db404fb5f478078d16fe
OskarEmanuelssonEdu <49032712+TheVolcanicSeal@users.noreply.github.com>
2021-04-14 12:57:03 +0200

    Update Bullet.h

--- Game/Bullet.h
+++ Game/Bullet.h
@@ -27,7 +27,7 @@ private:
 	Vector2 myDirection;
 	std::shared_ptr<Collider> myCollider;
 	float mySpeed = 0.0001f;
-	float myMaxLifetime = 20;
+	float myMaxLifetime = 10;
 	float mySize = 0.001;
 	float myRemainingLifetime;
 	bool myIsActive;	
