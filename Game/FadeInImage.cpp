6c828198b7d66ec01a63cbc2e94a5fcf83358045
VictorPetersson3 <victor.petersson@studerande.thegameassembly.se>
2021-04-21 14:33:51 +0200

    Logo Fixed

--- Game/FadeInImage.cpp
+++ Game/FadeInImage.cpp
@@ -18,7 +18,7 @@ void FadeInImage::Init(const EStateType& aState)
 
 	myTga2DBackground = std::make_unique<UIImage>();
 	myTga2DBackground->Init({ 0.5f, 0.5f }, "sprites/tga_logo.dds", 2);
-	myTga2DBackground->GetRenderCommand().SetSizeRelativeToImage({ 1.0f, 1.0f });
+	myTga2DBackground->GetRenderCommand().SetSizeRelativeToImage({ 0.5f, 0.5f });
 
 	myGameLogoBackground = std::make_unique<UIImage>();
 	myGameLogoBackground->Init({ 0.5f, 0.5f }, "sprites/UI/GroupLogo_Colour.dds", 2);
